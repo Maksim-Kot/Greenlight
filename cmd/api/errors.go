dd5eb91fe5411355259e13e592e7d98060d8a495
Maksim Kot <kotmaksimarturovich@gmail.com>
2024-09-27 02:06:41 +0300

    Implemented a permission-based authorization pattern. Added migrations files. Created functions for permissions retrieving. Updated models. Created new middlewares for checking authorization, activation and permissions. Added new custom error responces

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -87,3 +87,24 @@ func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter
 	message := "invalid or missing authentication token"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+// The authenticationRequiredResponse() method will be used to send a 401 Unauthorized response
+// status code and JSON response to the client.
+func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
+	message := "you must be authenticated to access this resource"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+// The inactiveAccountResponse() method will be used to send a 403 Forbidden response
+// status code and JSON response to the client.
+func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account must be activated to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
+
+// The notPermittedResponse() method will be used to send a 403 Forbidden response
+// status code and JSON response to the client.
+func (app *application) notPermittedResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account doesn't have the necessary permissions to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
